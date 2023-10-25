5f5dbdf2f0e59f177906bf00a3d0e90672ad3a06
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2023-10-25 10:42:38 +0200

    first

---
+++ C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // output
+
+            // input
+
+            // types
+
+            // logic and armethic
+
+            // for loop and if
+
+            // initaliztion and array
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
